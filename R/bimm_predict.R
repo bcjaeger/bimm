f8478b8b8c174e4204010b45da0d23eb6cefb8af
byron jaeger <byron.jaeger@gmail.com>
2021-10-15 13:44:40 -0400

    issues #2 and #4

---
+++ R/bimm_predict.R
@@ -0,0 +1,36 @@
+
+
+
+#' Title
+#'
+#' @param object
+#' @param new_data
+#' @param type
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+bimm_predict <- function(object,
+                         new_data,
+                         type = 'new_obs'){
+
+  if(type == 'new_sub'){
+    p <- predict(object = object$model_ml,
+                 data = new_data,
+                 type = 'response')
+    return(p$predictions[, 2, drop=TRUE])
+  }
+
+  if(type == 'new_obs'){
+
+    tmp <- data.frame(._x_. = object$fun_pred_ml(object$model_ml))
+
+    predict(object = object$model_mer,
+            newdata = cbind(tmp, new_data),
+            re.form = NULL)
+
+  }
+
+}
