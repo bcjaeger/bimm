125b105bc65230d3c94c90a56e50b2f3898fd0e2
Jaime <jspeiser@wakehealth.edu>
2024-09-04 14:34:53 -0400

    Update bimm_predict.R

--- R/bimm_predict.R
+++ R/bimm_predict.R
@@ -16,16 +16,17 @@ bimm_predict <- function(object,
                          new_data,
                          type = 'new_obs'){
 
-  if(type == 'new_sub'){
-    p <- predict(object = object$model_ml,
+  p <- predict(object = object$model_ml,
                  data = new_data,
                  type = 'response')
+
+  if(type == 'new_sub'){
     return(p$predictions[, 2, drop=TRUE])
   }
 
   if(type == 'new_obs'){
 
-    tmp <- data.frame(._x_. = object$fun_pred_ml(object$model_ml))
+    tmp <- data.frame(._x_. = p$predictions[, 2, drop=TRUE])
 
     predict(object = object$model_mer,
             newdata = cbind(tmp, new_data),
